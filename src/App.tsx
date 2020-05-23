dc2c2ce64f4b60e8e75835faea93098609f0df15
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-23 11:40:17 -0300

    Add SigIn Index structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import GlobalStyle from './stles/global';
+import SignIn from './pages/Signin';
+import GlobalStyle from './sytles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
