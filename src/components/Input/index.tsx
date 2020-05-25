ac6e3a0559af6cddbcc0b53fd9ec4eb10d3311d9
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-25 17:30:36 -0300

    Add UseFild, UseEfect and UseRef. accessing typed information

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,37 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 
 import { Container } from './styles';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 // Cria Interface p trazer tds propriedades do Input do CSS p dentro do Componente Input
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  // Sobre escrevo name
+  // Sobre-escrevo name
   name: string;
   // recebo um componente como uma propriedade
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 // Passo como parametro InputProps, e assim posso utiolizar tds as propriedades q o Input tem dentro do componente
 // repassando as Props p o Input
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest}) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    })
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input ref={inputRef} {...rest} />
   </Container>
-);
+  );
+};
 
 export default Input;
