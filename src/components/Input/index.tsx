8511b3ce7d75ad2789dfdaf98c4106fd9ec69735
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-26 16:21:20 -0300

    Add show Error to input. Static.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -60,6 +60,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
         ref={inputRef}
         {...rest}
       />
+
+      {error}
   </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
@@ -11,6 +12,8 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
       //p validar um obj inteiro cria um schema de validaçao
@@ -23,11 +26,13 @@ const SignUp: React.FC = () => {
       });
 
       await schema.validate(data, {
-        //para eniar tds os erros e não só o 1°
+        //para enviar tds os erros e não só o 1°
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      formRef.current?.setErrors({
+        email: 'Nome Obrigatorio',
+      });
     }
   }, []);
 
@@ -38,13 +43,11 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logo} alt="GoBarber"/>
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name='nome' icon={FiUser} placeholder="Nome" />
-
           <Input name='email' icon={FiMail} placeholder="E-mail" />
-
           <Input name='password' icon={FiLock} type="password" placeholder="Senha" />
 
           <Button type="submit">Cadastrar</Button>
