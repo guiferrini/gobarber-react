30fd59befdd4dd505bbd662c86b5a0ebd5cd5751
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-27 17:02:30 -0300

    Add context API to SignIn. Static.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-//import SignIn from './pages/Signin';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
+//import SignUp from './pages/SignUp';
 import GlobalStyle from './sytles/global';
+
+import AuthContext from './context/AuthContext';
+//AuthContext.provider -> tem q estar em volta de td o q será autenticado
 
 const App: React.FC = () => (
   <>
-    <SignUp />
-    <GlobalStyle />
+    <AuthContext.Provider value={{ name: 'gui' }}>
+      <SignIn />
+    </AuthContext.Provider>
+      <GlobalStyle />
   </>
 );
 
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
+import getValidationErrors from '../../Utils/getValitationErrors';
 
 import logo from '../../Assets/logo.svg';
 
@@ -8,31 +14,64 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logo} alt="GoBarber"/>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu Login</h1>
+  const auth = useContext(AuthContext);
 
-        <Input name='email' icon={FiMail} placeholder="E-mail" />
+  console.log(auth);
 
-        <Input name='password' icon={FiLock} type="password" placeholder="Senha" />
+  const handleSubmit = useCallback(async (data: object) => {
+    //p validação ser feita do zero
+    formRef.current?.setErrors({});
 
-        <Button type="submit">Entrar</Button>
+    try {
+      //p validar um obj inteiro cria um schema de validaçao
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+      await schema.validate(data, {
+        //para enviar tds os erros e não só o 1°
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
 
-      <a href="Login">
-        <FiLogIn />
-        Criar Conta
-      </a>
+      formRef.current?.setErrors(errors);
+
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GoBarber"/>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Login</h1>
+
+          <Input name='email' icon={FiMail} placeholder="E-mail" />
+
+          <Input name='password' icon={FiLock} type="password" placeholder="Senha" />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="Login">
+          <FiLogIn />
+          Criar Conta
+        </a>
     </ Content>
 
-    <Background />
-  </ Container>
-);
+      <Background />
+    </ Container>
+  );
+}
 
 export default SignIn;
