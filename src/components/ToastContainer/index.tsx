f26e9f07eac07ada8f2a312a0ab0164794c67ab6
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-28 17:06:47 -0300

    Add Toast Error Message: Styles. Static.

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -6,7 +6,32 @@ import { Container, Toast } from './styles';
 const ToastContainer: React.FC = () => {
   return (
     <Container>
-      <Toast>
+      <Toast hasDescription>
+        <FiAlertTriangle size={18} />
+
+        <div>
+          <strong>Título do ERRO</strong>
+          <p>Descrição do ERRO</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="success" hasDescription={false}>
+        <FiAlertTriangle size={18} />
+
+        <div>
+          <strong>Título do ERRO</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription>
         <FiAlertTriangle size={18} />
 
         <div>
